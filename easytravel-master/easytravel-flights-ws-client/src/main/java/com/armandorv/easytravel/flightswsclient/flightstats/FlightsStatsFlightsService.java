b8baff74e7c131e21a68b5dff590a0be51c9e80b
armandorvila <armando.ramirez.vila@gmail.com>
2013-03-18 19:53:55 +0100

    Completed flights tests.

--- easytravel-master/easytravel-flights-ws-client/src/main/java/com/armandorv/easytravel/flightswsclient/flightstats/FlightsStatsFlightsService.java
+++ easytravel-master/easytravel-flights-ws-client/src/main/java/com/armandorv/easytravel/flightswsclient/flightstats/FlightsStatsFlightsService.java
@@ -15,44 +15,53 @@ import com.armandorv.easytravel.flightswsclient.exception.FlightsException;
 import com.armandorv.easytravel.flightswsclient.model.Flight;
 import com.conducivetech.cache.flighthistory.api_internal.v2.APIException_Exception;
 import com.conducivetech.cache.flighthistory.api_internal.v2.AirportService;
+import com.conducivetech.cache.flighthistory.api_internal.v2.AirportV1;
 import com.conducivetech.cache.flighthistory.api_internal.v2.FlightStatusV2;
+import com.conducivetech.cache.flighthistory.api_internal.v2.FlightStatusesResponse.FlightStatuses;
 import com.conducivetech.cache.flighthistory.api_internal.v2.ResponseAirportStatus;
 
 @Component
 class FlightsStatsFlightsService implements FlightsService {
 
+	private static final String ICAO_TYPE = "ICAO";
+
 	private static Logger log = Logger
 			.getLogger(FlightsStatsFlightsService.class);
 
-	@Value("flightstats.appId")
+	private static final int HOUR_0 = 0;
+	private static final int HOUR_7 = 7;
+	private static final int HOUR_14 = 14;
+	private static final int HOUR_20 = 20;
+
+	@Value("$fli{flightstats.appId}")
 	private String appId;
 
-	@Value("flightstats.appKey")
+	@Value("$fli{flightstats.appKey}")
 	private String appKey;
 
 	@Autowired
 	private AirportService port;
 
+	private List<AirportV1> airports = new ArrayList<>();
+
 	/**
-	 * Get all flights between 10AM and 16PM for the given airport.
+	 * Get all flights between 9AM and 3PM for the given airport.
 	 */
 	public List<Flight> findFlights(String ICAO) throws FlightsException {
-
 		try {
-
-			DateTime today = new DateTime();
-			int day = today.getDayOfMonth();
-			int month = today.getMonthOfYear();
-			int year = today.getYear();
-			int hourOfDay = 9;
-
 			log.debug("Invoking flight stats service.");
 
-			ResponseAirportStatus response = port.airportStatusDep(appId,
-					appKey, ICAO, year, month, day, hourOfDay, false, 6, null,
-					"ICAO", null, null);
+			FlightStatuses response = invoke(ICAO, HOUR_0);
+			FlightStatuses response2 = invoke(ICAO, HOUR_7);
+			FlightStatuses response3 = invoke(ICAO, HOUR_14);
+			FlightStatuses response4 = invoke(ICAO, HOUR_20);
 
-			return flights(response.getFlightStatuses().getFlightStatus());
+			List<Flight> flights = flights(response.getFlightStatus());
+			flights.addAll(flights(response2.getFlightStatus()));
+			flights.addAll(flights(response3.getFlightStatus()));
+			flights.addAll(flights(response4.getFlightStatus()));
+
+			return flights;
 
 		} catch (APIException_Exception e) {
 			log.error("Error invoking flight stats service :" + e.getMessage(),
@@ -61,6 +70,22 @@ class FlightsStatsFlightsService implements FlightsService {
 					+ e.getMessage());
 		}
 
+	}
+
+	private FlightStatuses invoke(String ICAO, int hourOfDay)
+			throws APIException_Exception {
+
+		DateTime today = new DateTime();
+		int day = today.getDayOfMonth();
+		int month = today.getMonthOfYear();
+		int year = today.getYear();
+
+		ResponseAirportStatus response = port.airportStatusDep(appId, appKey,
+				ICAO, year, month, day, hourOfDay, false, 6, null, ICAO_TYPE,
+				null, null);
+
+		airports.addAll(response.getAppendix().getAirports().getAirport());
+		return response.getFlightStatuses();
 	}
 
 	private List<Flight> flights(List<FlightStatusV2> flightStatuses) {
@@ -76,17 +101,20 @@ class FlightsStatsFlightsService implements FlightsService {
 	private Flight flight(FlightStatusV2 flightStatus) {
 		Flight flight = new Flight();
 
+		AirportV1 destiny = findAirport(flightStatus.getArrivalAirportFsCode());
+		AirportV1 origin = findAirport(flightStatus.getDepartureAirportFsCode());
+
 		flight.setId(flightStatus.getFlightId() + "");
 
-		flight.setDestinationAirportICAO(flightStatus.getDepartureAirport()
-				.getIcao());
-		flight.setDestinationCity(flightStatus.getDepartureAirport().getCity());
-		flight.setDestinationAirportName(flightStatus.getDepartureAirport()
-				.getName());
+		flight.setDestinationAirportICAO(destiny != null ? destiny.getIcao()
+				: "");
+		flight.setDestinationCity(destiny != null ? destiny.getCity() : "");
+		flight.setDestinationAirportName(destiny != null ? destiny.getName()
+				: "");
 
-		flight.setOriginAirportICAO(flightStatus.getArrivalAirport().getIcao());
-		flight.setOriginAirportName(flightStatus.getArrivalAirport().getCity());
-		flight.setOriginCity(flightStatus.getArrivalAirport().getName());
+		flight.setOriginAirportICAO(origin != null ? origin.getIcao() : "");
+		flight.setOriginAirportName(origin != null ? origin.getCity() : "");
+		flight.setOriginCity(origin != null ? origin.getName() : "");
 
 		// All flights have today
 		flight.setDepartureTime(new Date());
@@ -94,4 +122,17 @@ class FlightsStatsFlightsService implements FlightsService {
 
 		return flight;
 	}
+
+	private AirportV1 findAirport(String arrivalAirportFsCode) {
+		if (arrivalAirportFsCode == null)
+			return null;
+		
+		for (AirportV1 airport : airports) {
+			if (arrivalAirportFsCode.equals(airport.getFs()))
+				return airport;
+		}
+
+		return null;
+	}
+
 }
--- easytravel-master/easytravel-flights-ws-client/src/test/java/com/armandorv/easytrave/flightswsclient/flightware/FlightsServiceTest.java
+++ easytravel-master/easytravel-flights-ws-client/src/test/java/com/armandorv/easytrave/flightswsclient/flightware/FlightsServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -26,9 +25,6 @@ public class FlightsServiceTest {
 
 	@Autowired
 	private FlightsService flightsService;
-
-	@Autowired
-	private ApplicationContext applicationContext;
 
 	@Before
 	public void setUp() {
